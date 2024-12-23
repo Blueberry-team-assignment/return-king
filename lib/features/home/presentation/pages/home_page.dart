617e5fcb9ad68dd246293c17a558903e39aed643
positive port <40537312+hangyuCho@users.noreply.github.com>
2024-12-24 08:56:58 +0900

    홈 템플릿 파일 제거 후 홈 페이지에서 컨포넌트를 작성함.
    각 경로의 컨포넌트 별 역할
    (현재 특정 부분은 페이지와 템플릿의 역할을 동시에 진행)
    * 페이지 : 라우팅과 스테이트를 관리하는 것
    * 템플릿 :  페이지의 레이아웃을 조정하는 것
    * 올가니즘 : 단독 컨포넌트
    * 몰리큘스 : 보조 컨포넌트
    * 아톰 : 최소 단위의 컨포넌트

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:return_king/features/home/presentation/templates/home_template.dart';
+import 'package:return_king/features/home/presentation/molecules/room_list_tab.dart';
+import 'package:return_king/features/home/presentation/molecules/timeline_tab.dart';
+import 'package:return_king/features/home/providers/home_providers.dart';
+import 'package:return_king/features/room/providers/room_providers.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const HomeTemplate();
+    final currentTabIndex = ref.watch(homeTabProvider);
+    final rooms = ref.watch(roomListProvider);
+    final hasRooms = rooms.isNotEmpty;
+    return Scaffold(
+      appBar: AppBar(title: const Text('답례왕')),
+      floatingActionButton: ElevatedButton(
+          onPressed: () => {
+                if (hasRooms)
+                  {ref.read(roomListProvider.notifier).clearRooms()}
+                else
+                  {ref.read(roomListProvider.notifier).fetchRooms()}
+              },
+          child: Text('hasRooms : ${!hasRooms}')),
+      body: IndexedStack(
+        index: currentTabIndex,
+        children: const [
+          RoomListTab(),
+          TimelineTab(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentTabIndex,
+          onTap: (index) {
+            ref.read(homeTabProvider.notifier).state = index;
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: ''),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.timeline_rounded), label: ''),
+          ]),
+    );
   }
 }
