ca739ebc1ba50d616209bfc563785b592a730d91
hangyuCho <chg3376@gmail.com>
2024-12-26 15:27:12 +0900

    add : firebase settings

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:return_king/features/home/presentation/pages/home_page.dart';
+import 'package:return_king/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
