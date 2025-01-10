0738994a07eeaf2e4247cbbcdc6ee8f0031c5541
hangyuCho <chg3376@gmail.com>
2025-01-10 09:05:23 +0900

    fix : layout for room card

--- lib/features/room/presentation/molecules/room_card.dart
+++ lib/features/room/presentation/molecules/room_card.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
+import 'package:return_king/features/room/domain/enums/sender_type.dart';
 import 'package:return_king/features/room/domain/models/room.dart';
 import 'package:return_king/features/room/domain/providers/room_providers.dart';
 import 'package:return_king/features/room/presentation/pages/room_detail_page.dart';
 import 'package:return_king/features/timeline/domain/providers/timeline_providers.dart';
-import 'package:return_king/shared/presentation/atoms/avatar.dart';
 
 class RoomCard extends ConsumerWidget {
   const RoomCard({
@@ -18,62 +20,84 @@ class RoomCard extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return InkWell(
       onTap: () async {
-        /// 상세 페이지로 이동하기 전에 데이터 취득 처리 실행
-        // 선택된 룸과 이어지는 타임라인을 취득
         if (room.id == null) {
           throw Exception('not selected Room');
         }
+
+        /// 데이터 가져오기
         await ref.read(selectedRoomProvider.notifier).fetchRoom(room.id!);
-        // room별 타임라인 취득
-        if (room.id != null) {
-          await ref
-              .read(selectedTimelineListByRoomIdProvider.notifier)
-              .fetchTimelineByRoomId(room.id!);
+        await ref
+            .read(selectedTimelineListByRoomIdProvider.notifier)
+            .fetchTimelineByRoomId(room.id!);
+
+        /// 안전하게 context 확인 후 화면 이동
+        if (context.mounted) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const RoomDetailPage()),
+          );
         }
-        // 상세 페이지로 이동
-        // ignore: use_build_context_synchronously
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => const RoomDetailPage()));
       },
-      child: Card(
-        color: Colors.indigo[200], // Card自体の色
-        margin: const EdgeInsets.all(2),
-        shadowColor: Colors.black, // 影の色
-        shape: RoundedRectangleBorder(
-          // 枠線を変更できる
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          children: [
-            Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-                child: Row(
-                  children: [
-                    const Avatar(),
-                    const SizedBox(width: 12),
-                    Column(
-                      children: [
-                        Text(
-                          room.lastTimeline?.createdAt.toString() ?? '',
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Card(
+          color: Colors.indigo[50],
+          margin: const EdgeInsets.all(2),
+          shadowColor: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 8.0, horizontal: 10.0),
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Icon(Icons.wallet_giftcard),
+                      const SizedBox(width: 12),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              room.lastTimeline?.senderType ==
+                                      SenderType.receiver
+                                  ? room.name
+                                  : '내가 준 선물',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 14.0,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                            ),
+                            Text(
+                              room.lastTimeline?.content ??
+                                  '타임라인이 없습니다.',
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            ),
+                          ],
                         ),
-                        Text(
-                          room.name,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                        ),
-                        Text(
-                          room.lastTimeline?.content ?? '',
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                        ),
-                      ],
-                    )
-                  ],
-                ))
-          ],
+                      ),
+                      Text(
+                        room.lastTimeline?.createdAt != null
+                            ? DateFormat('yy/MM/dd')
+                                .format(room.lastTimeline!.createdAt)
+                            : '날짜 없음',
+                        textAlign: TextAlign.right,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
