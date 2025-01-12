167e51b558eda853a5d86c8c428079a38b97f61b
positive port <40537312+hangyuCho@users.noreply.github.com>
2025-01-12 21:30:56 +0900

    Refactor UI and bugfix (#39)
    * fix : ui and bugfix
    
    * fix : build error
    
    * fix : ui fix
    
    * fix : update readme

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -6,21 +6,23 @@ import 'package:return_king/features/home/presentation/molecules/room_list_tab.d
 import 'package:return_king/features/home/presentation/molecules/timeline_tab.dart';
 import 'package:return_king/features/home/domain/providers/home_providers.dart';
 import 'package:return_king/features/room/domain/providers/room_providers.dart';
+import 'package:return_king/features/room/presentation/atoms/buttons/person_button.dart';
+import 'package:return_king/features/room/presentation/atoms/buttons/plus_button.dart';
+import 'package:return_king/features/room/presentation/pages/room_detail_page.dart';
 import 'package:return_king/features/timeline/domain/providers/timeline_providers.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final currentTabIndex = ref.watch(homeTabProvider);
-    final rooms = ref.watch(roomListProvider);
-    final hasRooms = rooms.isNotEmpty;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('답례왕'),
-        actions: [
-          ElevatedButton(
+
+  Future<void> _dialogBuilder(BuildContext context,WidgetRef ref) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: const Text('정말 로그아웃 하시겠습니까?'),
+          actions: <Widget>[
+            ElevatedButton(
               onPressed: () async {
                 ref.invalidate(homeTabProvider); // 초기화
                 ref.invalidate(roomListProvider); // 초기화
@@ -29,19 +31,48 @@ class HomePage extends ConsumerWidget {
                   context: context,
                   auth: FirebaseAuth.instance,
                 );
+                // ignore: use_build_context_synchronously
+                Navigator.of(context).pop();
               },
-              child: const Text('로그아웃'))
+              child: const Text('로그아웃')),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('닫기'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentTabIndex = ref.watch(homeTabProvider);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('답례왕'),
+        elevation: 2.0,
+        leading: 
+          PlusButton(onPressed: () {
+            ref
+                .read(selectedTimelineListByRoomIdProvider.notifier)
+                .clearSelectedRoom();
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        const RoomDetailPage(isNewRoom: true)));
+          }),
+        actions: [
+          PersonButton(onPressed: () => _dialogBuilder(context, ref)),
         ],
       ),
-      floatingActionButton: ElevatedButton(
-          onPressed: () {
-            if (hasRooms) {
-              ref.read(roomListProvider.notifier).clearRooms();
-            } else {
-              ref.read(roomListProvider.notifier).fetchRooms();
-            }
-          },
-          child: Text('hasRooms : ${!hasRooms}')),
       body: IndexedStack(
         index: currentTabIndex,
         children: const [
